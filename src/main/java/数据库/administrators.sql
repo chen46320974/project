0529dedeef39e8afdd20695f729df4a92326a2a5
25030 <2503090710@qq.com>
2019-12-29 21:13:43 +0800

    数据库数据传上来了，在target文件里

---
+++ src/main/java/数据库/administrators.sql
@@ -0,0 +1,2 @@
+INSERT INTO `administrators` VALUES ('0302', 'Sanji', '19983287393', 'GreenAlgaeHead', '19991111');
+INSERT INTO `administrators` VALUES ('1111', 'Zoro', '19983287393', 'CurlyEyebrowsGoat', '19990302');
